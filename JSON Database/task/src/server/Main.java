3818d0529cf183d5aa856a79721edd6d97ef461b
lem <lemrey@gmail.com>
2021-01-03 18:02:43 +0100

    Add message to main

--- JSON Database/task/src/server/Main.java
+++ JSON Database/task/src/server/Main.java
@@ -3,6 +3,7 @@ package server;
 public class Main {
 
     public static void main(String[] args) {
+	System.out.println("Ricchietta");
         Server server = new Server();
         server.startServer();
     }
